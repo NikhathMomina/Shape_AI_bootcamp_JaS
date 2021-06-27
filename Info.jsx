ae055ec47dd81fed6f31cbb680db18e225eb0110
NikhathMomina <86521876+NikhathMomina@users.noreply.github.com>
2021-06-27 17:25:28 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and Reactjs by shaurya siNnha</h1>
+      <p>
+        Very informative session helped us in gaining a lot of knowledge
+      </p>
+    </div>
+  );
+}
+export default Info;
